a1b696a5af69660fe03abaa7658361b08c072662
pepsm <peps.mihailov@gmail.com>
2021-07-02 17:10:58 +0200

    Update: Set productionServer proxy

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -2,12 +2,22 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
     proxy: {
-      '^/api':{
+      "^/api": {
         target: "https://api.publicapis.org",
         changeOrigin: true,
-        logLevel: 'debug',
-        pathRewrite: {'^/api': '/'}
-      }
-    }
-  }
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
+  proServer: {
+    proxy: {
+      "^/api": {
+        target: "https://api.publicapis.org",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
 };
